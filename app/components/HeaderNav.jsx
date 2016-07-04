b4e73db0165138b0f36af2f7d0e2588bde132fa3
DanielFGray <DanielFGray@gmail.com>
2016-07-04 14:50:55 -0500

    add explanation dialog

--- app/components/HeaderNav.jsx
+++ app/components/HeaderNav.jsx
@@ -3,6 +3,8 @@ import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import Divider from 'material-ui/Divider';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 import { Link } from 'react-router';
 
 export default class HeaderNav extends Component {
@@ -12,42 +14,70 @@ export default class HeaderNav extends Component {
     super(props);
     this.state =
       { drawerOpen: ! this.props.title
+      , dialogOpen: false
       };
   }
 
-  handleToggle = () => this.setState({ drawerOpen: ! this.state.drawerOpen })
+  drawerToggle = () => this.setState({ drawerOpen: ! this.state.drawerOpen })
 
-  handleClose = () => this.setState({ drawerOpen: false })
+  drawerClose = () => this.setState({ drawerOpen: false })
 
-  handleClick = () => {
-    this.handleClose();
-  }
+  dialogOpen = () => this.setState({ dialogOpen: true })
+
+  dialogClose = () => this.setState({ dialogOpen: false })
 
   render() {
     return (
       <div id="content">
         <AppBar
           title={this.props.title}
-          onLeftIconButtonTouchTap={this.handleToggle}
+          onLeftIconButtonTouchTap={this.drawerToggle}
         />
+        <Dialog
+          actions={[
+            <FlatButton
+              label="you're a nerd"
+              primary={true}
+              keyboardFocused={true}
+              onTouchTap={this.dialogClose}
+            />
+          ]}
+          modal={false}
+          open={this.state.dialogOpen}
+          onRequestClose={this.handleClose}
+        >
+          <p>
+            I made an IRC chat bot that responds to certain keywords in some IRC
+            channels on rizon.net. This means that if you say a defined trigger word,
+            the bot will automatically respond with the appropriate factoid.
+            <br />It's hard to remember all of the factoids it has, so I made
+            a website to list all of them for each channel.
+          </p>
+          <p>It's also been a fun excuse to play with <a href="http://github.com/facebook/react">React</a> and
+            the <a href="https://github.com/callemall/material-ui">Material-UI</a> libraries.
+          </p>
+        </Dialog>
         <Drawer
           docked={false}
           width={200}
           open={this.state.drawerOpen}
           onRequestChange={(drawerOpen) => this.setState({ drawerOpen })}
         >
-          <MenuItem onTouchTap={this.handleClick} containerElement={<Link to="/rice" />}>
+          <MenuItem onTouchTap={this.drawerClose} containerElement={<Link to="/rice" />}>
             #rice
           </MenuItem>
-          <MenuItem onTouchTap={this.handleClick} containerElement={<Link to="/code" />}>
+          <MenuItem onTouchTap={this.drawerClose} containerElement={<Link to="/code" />}>
             #code
           </MenuItem>
-          <MenuItem onTouchTap={this.handleClick} containerElement={<Link to="/wdg" />}>
+          <MenuItem onTouchTap={this.drawerClose} containerElement={<Link to="/wdg" />}>
             #/g/wdg
           </MenuItem>
           <Divider />
-          <MenuItem onTouchTap={this.handleClick} containerElement={<a href="https://gitlab.com/DanielFGray/factoidlist" target="_blank" />}>
+          <MenuItem onTouchTap={this.drawerClose} containerElement={<a href="https://gitlab.com/DanielFGray/factoidlist" target="_blank" />}>
             source
+          </MenuItem>
+          <MenuItem onTouchTap={this.dialogOpen}>
+            what is this?
           </MenuItem>
         </Drawer>
       </div>
