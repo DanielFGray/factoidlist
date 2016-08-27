7a505a2ebe6a9a8560ed39ec50a02141cb11098b
DanielFGray <DanielFGray@gmail.com>
2016-08-27 08:48:40 +0200

    remove marked and parse `code` and links manually

--- app/components/Factoid.js
+++ app/components/Factoid.js
@@ -1,35 +1,37 @@
 import React, { PropTypes } from 'react';
 import { TableRow
        , TableRowColumn } from 'material-ui/Table';
-import marked from 'marked';
 import moment from 'moment';
 
-marked.setOptions(
-  { renderer: new marked.Renderer()
-  , gfm: true
-  , tables: false
-  , breaks: false
-  , pedantic: true
-  , sanitize: true
-  , smartLists: false
-  , smartypants: false
-  }
-);
+function linkify(inputText) {
+  // http://stackoverflow.com/a/3890175
+  const replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+  let replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+  const replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+  replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+  const replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+  replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+  return replacedText;
+}
 
 export default function Factoid(props) {
   const time = moment(props.time);
+  const fact = linkify(props.fact).replace(/`([^`]+)`/, '<code>$1</code>');
+
   return (
     <TableRow key={props.name}>
       <TableRowColumn style={{ width: '12em' }}>
         {props.name.length > 0 ? `!${props.name}` : ''}
       </TableRowColumn>
       <TableRowColumn
-        dangerouslySetInnerHTML={{ __html: marked(props.fact) }}
         style={{ height: 'unset'
                , textOverflow: 'unset'
                , whiteSpace: 'unset'
+               , padding: '5px'
                }}
-      />
+      >
+        <div dangerouslySetInnerHTML={{ __html: fact }} />
+      </TableRowColumn>
       <TableRowColumn style={{ width: '15em', fontSize: 'smaller', textAlign: 'right' }} >
         <div>{props.nick}</div>
         <div>
