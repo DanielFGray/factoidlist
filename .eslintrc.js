27305347ab5334c400938c78a22639056fa6adea
DanielFGray <DanielFGray@gmail.com>
2017-05-22 19:44:14 -0500

    convert eslintrc to js

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+  },
+  rules: {
+    'no-console': 0,
+    'react/jsx-boolean-value': 0,
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'no-nested-ternary': 0,
+    'react/jsx-filename-extension': [ 'error', { 'extensions': [ '.js', '.jsx' ] } ],
+    'react/forbid-prop-types': 'warn',
+    'jsx-a11y/anchor-has-content': 0,
+    'space-unary-ops': [
+      2, {
+        words: true,
+        nonwords: false,
+        overrides: {
+          '!': true,
+        },
+      },
+    ],
+  },
+};
